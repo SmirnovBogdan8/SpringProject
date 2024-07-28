f4ea9ad7e463895ce2047c9b733ec2c11e06868f
Smirnovbogdan8 <Smirnovbogdan8@gmail.com>
2024-07-28 15:16:36 +0300

    Исправление ошибок

--- src/main/java/org/example/fileProcessingSystem/service/SecondaryValidationService.java
+++ src/main/java/org/example/fileProcessingSystem/service/SecondaryValidationService.java
@@ -1,34 +1,5 @@
 package org.example.fileProcessingSystem.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-import org.example.fileProcessingSystem.common.StatusService;
 
-@Service
 public class SecondaryValidationService {
-
-    @Autowired
-    private StatusService statusService;
-
-    @KafkaListener(topics = "validation-topic", groupId = "secondary-validation-group")
-    public void listen(String fileName) {
-        // Логика вторичной валидации (например, проверка содержимого файла)
-        boolean isValid = performSecondaryValidation(fileName);
-
-        if (isValid) {
-            // Обновление статуса в MongoDB
-            statusService.updateStatus(fileName, "Passed secondary validation");
-        } else {
-            // Обновление статуса в MongoDB
-            statusService.updateStatus(fileName, "Failed secondary validation");
-        }
-    }
-
-    private boolean performSecondaryValidation(String fileName) {
-        // Пример логики валидации: содержимое файла должно удовлетворять определенным условиям
-        // Здесь мы предполагаем, что файл уже загружен и доступен для чтения
-        // Логика проверки содержимого файла
-        return true; // Замените на реальную логику проверки содержимого
-    }
 }
